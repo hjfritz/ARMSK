d0c978a1f1b6f5edb0a7c5c1b0913dc41b32de3c
hjfritz <fritzhj@gmail.com>
2022-11-13 17:29:46 -0800

    Initial setup for iPhone build

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartShoulderExam()
+    {
+        menu.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
